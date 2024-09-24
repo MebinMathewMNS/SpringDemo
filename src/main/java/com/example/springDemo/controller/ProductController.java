addcd8285ff155639c94d24b75dc8d2cfde677a1
Mebin Mathew <mebin.mathew@marks-and-spencer.com>
2024-09-24 04:12:47 +0000

    Swagger changes

--- src/main/java/com/example/springDemo/controller/ProductController.java
+++ src/main/java/com/example/springDemo/controller/ProductController.java
@@ -12,24 +12,29 @@ public class ProductController {
 
     @Autowired
     ProductService productService;
+
     @GetMapping("/getMessage")
     public String getMessage(){
         return "Welcome to springDemo";
     }
+
     @GetMapping("/getProduct")
     public List<Product> getProduct(){
         return productService.getAllProduct();
     }
+
     @PostMapping("/addProduct/{id}")
     public boolean addProduct(@PathVariable Long id, @RequestBody Product product){
         productService.addProduct(id,product);
         return true;
     }
+
     @PutMapping("/updateProduct/{id}")
     public boolean updateProduct(@PathVariable Long id, @RequestBody Product product){
         productService.updateProduct(id,product);
         return true;
     }
+
     @DeleteMapping("/deleteProduct/{id}")
     public boolean deleteProduct(@PathVariable Long id){
         productService.deleteProduct(id);
